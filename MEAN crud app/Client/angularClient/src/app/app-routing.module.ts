fefda9b2b5ab9868f41f83a1ec34a979f4f46fac
jeeva.nikam <jeevann87@gmail.com>
2020-04-28 16:47:14 +0530

    Minor bug fix.

--- MEAN crud app/Client/angularClient/src/app/app-routing.module.ts
+++ MEAN crud app/Client/angularClient/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserComponent } from './user/user.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 import { RecordsComponent } from './records/records.component';
